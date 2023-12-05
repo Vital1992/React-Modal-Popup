9cae87830e18f8e604c1e4d6a97b8943658068ad
Vital1992 <vital09.92@gmail.com>
2023-12-05 15:14:19 -0800

    Fix chart

--- src/chart.js
+++ src/chart.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import { Chart, CategoryScale, LinearScale, LineController, LineElement, PointElement, Title } from 'chart.js';
+
+Chart.register(CategoryScale, LinearScale, LineController, LineElement, PointElement, Title);
 
 const LineChart = () => {
   // Define your data
